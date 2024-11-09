8d9ef43bcc2c7a0e5a0d9b64c500f4cf7851936f
karkigaurav675 <karkigaurav675@gmail.com>
2024-11-09 13:55:57 +0530

    deleted files

--- main.py
+++
@@ -1,9 +0,0 @@
-from src.signup_menu.signup_menu import RestaurantSystem
-from src.menu_mangment.menu_fecture import main
-restaurantSystem = RestaurantSystem()
-restaurantSystem.display_menu()
-
-menu = main()
-
-
-# menu = main()
